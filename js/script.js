ad1a3b813b98da2d9caf31fe068f9ee00fdfeba7
muhammadali0608 <muhammadalishuhratjonov61@gmail.com>
2022-05-07 08:23:42 +0500

    project completed

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@ const elHamburgerBtn = document.querySelector (".site-header__hamburger")
 const elHeader = document.querySelector (".site-header")
 
 elHamburgerBtn.addEventListener ("click", () => {
-    elHeader.classList.toggle ("site-header--open")
+    elHeader.classList.toggle (".site-header--open")
 })
